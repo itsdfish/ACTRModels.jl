de7246d2bffa225a23933f705d5b87b52d9a6991
itsdfish <itsdfish@gmail.com>
2020-08-26 05:38:18 -0400

    update version

--- src/Utilities/Turing Utilities.jl
+++ src/Utilities/Turing Utilities.jl
@@ -3,32 +3,32 @@ import Distributions: rand, logpdf, pdf, estimate
 function sampleChain(chain)
     parms = (Symbol.(chain.name_map.parameters)...,)
     idx = rand(1:length(chain))
-    vals = map(x->chain[x].value[idx],parms)
+    vals = map(x -> chain[x][idx], parms)
     return NamedTuple{parms}(vals)
 end
 
-function posteriorPredictive(m, chain, f=x->x)
+function posteriorPredictive(m, chain, f=x -> x)
     parms = sampleChain(chain)
     return f(m(parms))
 end
 
-function posteriorPredictive(model, chain, Nsamples::Int, f=x->x)
-    return map(x->posteriorPredictive(model, chain, f), 1:Nsamples)
+function posteriorPredictive(model, chain, Nsamples::Int, f=x -> x)
+    return map(x -> posteriorPredictive(model, chain, f), 1:Nsamples)
 end
 
 function reduceData(Data)
     U = unique(Data)
-    cnt = map(x->count(c->c==x, Data), U)
+    cnt = map(x -> count(c -> c == x, Data), U)
     newData = NamedTuple[]
-    for (u,c) in zip(U,cnt)
-        push!(newData, (u...,N=c))
+    for (u,c) in zip(U, cnt)
+        push!(newData, (u...,N = c))
     end
     return newData
 end
 
-function logNormParms(μ,σ)
-    μ′ = log(μ^2/sqrt(σ^2+μ^2))
-    σ′ = sqrt(log(1+σ^2/(μ^2)))
+function logNormParms(μ, σ)
+    μ′ = log(μ^2 / sqrt(σ^2 + μ^2))
+    σ′ = sqrt(log(1 + σ^2 / (μ^2)))
     return μ′,σ′
 end
 
@@ -55,7 +55,7 @@ findIndices(actr::ACTR; criteria...) = findIndices(actr.declarative.memory; crit
 function findIndices(chunks::Array{<:Chunk,1}; criteria...)
     idx = Int64[]
     for (i,c) in enumerate(chunks)
-        Match(c; criteria...) ? push!(idx,i) : nothing
+        Match(c; criteria...) ? push!(idx, i) : nothing
     end
     return idx
 end
@@ -65,7 +65,7 @@ findIndices(actr::ACTR, funs...; criteria...) = findIndices(actr.declarative.mem
 function findIndices(chunks::Array{<:Chunk,1}, funs...; criteria...)
     idx = Int64[]
     for (i,c) in enumerate(chunks)
-        Match(c, funs...; criteria...) ? push!(idx,i) : nothing
+        Match(c, funs...; criteria...) ? push!(idx, i) : nothing
     end
     return idx
 end
