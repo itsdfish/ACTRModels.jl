15160c47a04e89ef4a5e6a6950e0fee55dce9d2a
itsdfish <itsdfish@gmail.com>
2023-10-17 09:12:57 -0400

    remove unnecessary function

--- src/MemoryFunctions.jl
+++ src/MemoryFunctions.jl
@@ -1176,26 +1176,6 @@ function get_parm(actr::AbstractACTR, p)
 end
 
 """
-    blend_chunks(actr::AbstractACTR; request...) 
-
-Computes blended value over chunks given a retrieval request. By default, 
-values are blended over the set of slots formed by the set difference between all 
-slots of a chunk and the slots specified in the retrieval request. The default time used 
-in activation calculations is taken from `get_time`.
-
-# Arguments
-
-- `actr::AbstractACTR`: an `ACTR` model object 
-
-# Keywords
-
-- `request...`: optional keywords for the retrieval request
-"""
-function blend_chunks(actr::AbstractACTR; request...) 
-    return blend_chunks(actr, get_time(actr); request...) 
-end
-
-"""
     blend_chunks(actr::AbstractACTR, blended_slots; request...) 
 
 Computes blended value over chunks given a retrieval request. By default, 
